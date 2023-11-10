c0f8614f02af9fdd81f429c58535fe34d3accde5
josh16noronha <45009896+josh16noronha@users.noreply.github.com>
2023-11-10 16:01:38 -0500

    Added getBaseAmount and getTaxAmount methods to Site.

--- src/Site.java
+++ src/Site.java
@@ -1,11 +1,18 @@
 public abstract class Site {
-    public double TAX_RATE = 7;
-    double _units = 10;
-    double _rate= 15;
+    private double TAX_RATE = 7;
+    private double _units = 10;
+    private double _rate= 15;
 
-    double getBillableAmount(){
+    public double getBillableAmount(){
+        return getBaseAmount() + getTaxAmount();
+    }
+    public double getBaseAmount(){
         double base = _units * _rate;
-        double tax = base * TAX_RATE;
-        return base + tax;
-    };
+        return base;
+    }
+
+    public double getTaxAmount(){
+        double tax = getBaseAmount() * TAX_RATE;
+        return tax;
+    }
 }
